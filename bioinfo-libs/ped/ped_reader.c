1411a213df4cdae4c079e4632978596468975ed2
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-04-27 13:18:42 +0200

    Allow but ignore genotype columns

--- bioinfo-libs/ped/ped_reader.c
+++ bioinfo-libs/ped/ped_reader.c
@@ -22,7 +22,7 @@ static const int ped_error = 0;
 static const int ped_en_main = 1;
 
 
-#line 102 "ped.ragel"
+#line 107 "ped.ragel"
 
 
 
@@ -80,6 +80,7 @@ static void set_field(char* ts, char *te) {
         break;
     }
 
+//     LOG_DEBUG_F("Line %zu, current_field = %d\n", lines, current_field);
     current_field++;
 }
 
@@ -96,7 +97,7 @@ int ped_ragel_read(list_t *batches_list, size_t batch_size, ped_file_t *file)
     current_batch = ped_batch_new(batch_size);
 
     
-#line 100 "ped_reader.c"
+#line 101 "ped_reader.c"
 	{
 	cs = ped_start;
 	ts = 0;
@@ -104,25 +105,25 @@ int ped_ragel_read(list_t *batches_list, size_t batch_size, ped_file_t *file)
 	act = 0;
 	}
 
-#line 108 "ped_reader.c"
+#line 109 "ped_reader.c"
 	{
 	if ( p == pe )
 		goto _test_eof;
 	switch ( cs )
 	{
 tr0:
-#line 99 "ped.ragel"
+#line 104 "ped.ragel"
 	{te = p+1;}
 	goto st1;
 tr2:
-#line 72 "ped.ragel"
+#line 77 "ped.ragel"
 	{te = p+1;{
             // If batch is full, add to the list of batches and create a new, empty one
             if (ped_batch_is_full(current_batch))
             {
-                list_item_t *item = list_item_new(records, 1, current_batch); 
+                list_item_t *item = list_item_new(records, 1, current_batch);
                 list_insert_item(item, batches_list);
-                LOG_DEBUG_F("Batch added - %zu records", current_batch->length);
+                LOG_DEBUG_F("Batch added - %zu records\n", current_batch->length);
                 current_batch = ped_batch_new(batch_size);
             }
             
@@ -132,7 +133,7 @@ tr2:
                 records++;
             } else {
                 if (current_field > FAMILY_ID) {
-                    LOG_ERROR_F("Line %d - %d field(s) missing\n", lines, EOL - current_field);
+                    LOG_ERROR_F("Line %d - %d field(s) missing\n", lines, OTHER - current_field);
                     free(current_record);
                 }
             }
@@ -143,35 +144,23 @@ tr2:
             LOG_DEBUG("End of line\n");
         }}
 	goto st1;
-tr7:
-#line 70 "ped.ragel"
-	{te = p;p--;}
-	goto st1;
 tr8:
-#line 42 "ped.ragel"
+#line 49 "ped.ragel"
+	{te = p;p--;{
+            set_field(ts, te);
+        }}
+	goto st1;
+tr9:
+#line 45 "ped.ragel"
 	{te = p;p--;{
             LOG_DEBUG("Comment found, nothing to do.");
         }}
 	goto st1;
-tr9:
-#line 46 "ped.ragel"
-	{te = p;p--;{
-            set_field(ts, te);
-        }}
-	goto st1;
 tr11:
-#line 1 "NONE"
-	{	switch( act ) {
-	case 6:
-	{{p = ((te))-1;}
-            set_field(ts, te);
-        }
-	break;
-	default:
-	{{p = ((te))-1;}}
-	break;
-	}
-	}
+#line 73 "ped.ragel"
+	{te = p;p--;{
+            LOG_DEBUG("Genotypes found, nothing to do.");
+        }}
 	goto st1;
 st1:
 #line 1 "NONE"
@@ -181,24 +170,32 @@ st1:
 case 1:
 #line 1 "NONE"
 	{ts = p;}
-#line 185 "ped_reader.c"
+#line 174 "ped_reader.c"
 	switch( (*p) ) {
 		case 10: goto tr2;
+		case 32: goto tr0;
 		case 35: goto st3;
+		case 66: goto st6;
+		case 71: goto st7;
+		case 78: goto st7;
+		case 84: goto st7;
 		case 95: goto st6;
 	}
-	if ( (*p) < 58 ) {
-		if ( (*p) < 32 ) {
+	if ( (*p) < 65 ) {
+		if ( (*p) < 33 ) {
 			if ( 9 <= (*p) && (*p) <= 13 )
 				goto tr0;
 		} else if ( (*p) > 47 ) {
-			if ( 48 <= (*p) && (*p) <= 57 )
+			if ( (*p) > 57 ) {
+				if ( 58 <= (*p) && (*p) <= 64 )
+					goto st2;
+			} else if ( (*p) >= 48 )
 				goto st4;
 		} else
 			goto st2;
-	} else if ( (*p) > 64 ) {
+	} else if ( (*p) > 67 ) {
 		if ( (*p) < 91 ) {
-			if ( 65 <= (*p) && (*p) <= 90 )
+			if ( 68 <= (*p) && (*p) <= 90 )
 				goto st6;
 		} else if ( (*p) > 96 ) {
 			if ( (*p) > 122 ) {
@@ -209,7 +206,7 @@ case 1:
 		} else
 			goto st2;
 	} else
-		goto st2;
+		goto st7;
 	goto st0;
 st0:
 cs = 0;
@@ -218,27 +215,27 @@ st2:
 	if ( ++p == pe )
 		goto _test_eof2;
 case 2:
-	if ( 32 <= (*p) && (*p) <= 126 )
+	if ( 33 <= (*p) && (*p) <= 126 )
 		goto st2;
-	goto tr7;
+	goto tr8;
 st3:
 	if ( ++p == pe )
 		goto _test_eof3;
 case 3:
 	if ( 32 <= (*p) && (*p) <= 126 )
 		goto st3;
-	goto tr8;
+	goto tr9;
 st4:
 	if ( ++p == pe )
 		goto _test_eof4;
 case 4:
 	switch( (*p) ) {
-		case 46: goto tr10;
+		case 46: goto st5;
 		case 95: goto st6;
 	}
 	if ( (*p) < 65 ) {
 		if ( (*p) < 48 ) {
-			if ( 32 <= (*p) && (*p) <= 47 )
+			if ( 33 <= (*p) && (*p) <= 47 )
 				goto st2;
 		} else if ( (*p) > 57 ) {
 			if ( 58 <= (*p) && (*p) <= 64 )
@@ -256,33 +253,20 @@ case 4:
 			goto st6;
 	} else
 		goto st6;
-	goto tr9;
-tr10:
-#line 1 "NONE"
-	{te = p+1;}
-#line 70 "ped.ragel"
-	{act = 8;}
-	goto st5;
-tr12:
-#line 1 "NONE"
-	{te = p+1;}
-#line 62 "ped.ragel"
-	{act = 6;}
-	goto st5;
+	goto tr8;
 st5:
 	if ( ++p == pe )
 		goto _test_eof5;
 case 5:
-#line 277 "ped_reader.c"
 	if ( (*p) < 48 ) {
-		if ( 32 <= (*p) && (*p) <= 47 )
+		if ( 33 <= (*p) && (*p) <= 47 )
 			goto st2;
 	} else if ( (*p) > 57 ) {
 		if ( 58 <= (*p) && (*p) <= 126 )
 			goto st2;
 	} else
-		goto tr12;
-	goto tr11;
+		goto st5;
+	goto tr8;
 st6:
 	if ( ++p == pe )
 		goto _test_eof6;
@@ -291,7 +275,7 @@ case 6:
 		goto st6;
 	if ( (*p) < 65 ) {
 		if ( (*p) < 48 ) {
-			if ( 32 <= (*p) && (*p) <= 47 )
+			if ( 33 <= (*p) && (*p) <= 47 )
 				goto st2;
 		} else if ( (*p) > 57 ) {
 			if ( 58 <= (*p) && (*p) <= 64 )
@@ -309,7 +293,19 @@ case 6:
 			goto st6;
 	} else
 		goto st6;
-	goto tr9;
+	goto tr8;
+st7:
+	if ( ++p == pe )
+		goto _test_eof7;
+case 7:
+	switch( (*p) ) {
+		case 65: goto st7;
+		case 67: goto st7;
+		case 71: goto st7;
+		case 78: goto st7;
+		case 84: goto st7;
+	}
+	goto tr11;
 	}
 	_test_eof1: cs = 1; goto _test_eof; 
 	_test_eof2: cs = 2; goto _test_eof; 
@@ -317,23 +313,25 @@ case 6:
 	_test_eof4: cs = 4; goto _test_eof; 
 	_test_eof5: cs = 5; goto _test_eof; 
 	_test_eof6: cs = 6; goto _test_eof; 
+	_test_eof7: cs = 7; goto _test_eof; 
 
 	_test_eof: {}
 	if ( p == eof )
 	{
 	switch ( cs ) {
-	case 2: goto tr7;
-	case 3: goto tr8;
-	case 4: goto tr9;
-	case 5: goto tr11;
-	case 6: goto tr9;
+	case 2: goto tr8;
+	case 3: goto tr9;
+	case 4: goto tr8;
+	case 5: goto tr8;
+	case 6: goto tr8;
+	case 7: goto tr11;
 	}
 	}
 
 	_out: {}
 	}
 
-#line 177 "ped.ragel"
+#line 183 "ped.ragel"
  
 
     // Insert the last batch
@@ -345,24 +343,25 @@ case 6:
     }
 
     if ( cs < 
-#line 349 "ped_reader.c"
+#line 347 "ped_reader.c"
 1
-#line 187 "ped.ragel"
+#line 193 "ped.ragel"
  ) 
     {
+        LOG_ERROR("The file was not successfully read\n");
         LOG_INFO_F("Last state is %d, but %d was expected\n", 
                 cs, 
-#line 356 "ped_reader.c"
+#line 355 "ped_reader.c"
 1
-#line 190 "ped.ragel"
+#line 197 "ped.ragel"
 );
     } 
 
     LOG_INFO_F("Records read = %zu\n", records);
 
     return cs < 
-#line 365 "ped_reader.c"
+#line 364 "ped_reader.c"
 1
-#line 195 "ped.ragel"
+#line 202 "ped.ragel"
 ;
 }
