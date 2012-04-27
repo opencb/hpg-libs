60f71b6b0f78fdc51c0a88818f3bff43a748bc97
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-04-27 13:02:54 +0200

    New value for PED_Field added

--- bioinfo-libs/ped/ped_reader.h
+++ bioinfo-libs/ped/ped_reader.h
@@ -15,7 +15,7 @@
 #include "ped_read.h"
 #include "ped_batch.h"
 
-enum PED_Field { FAMILY_ID, INDIVIDUAL_ID, FATHER_ID, MOTHER_ID, SEX, PHENOTYPE };
+enum PED_Field { FAMILY_ID, INDIVIDUAL_ID, FATHER_ID, MOTHER_ID, SEX, PHENOTYPE, OTHER };
 
 int ped_ragel_read(list_t *batches_list, size_t batch_size, ped_file_t *file);
 
