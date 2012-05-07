7558af156bb853b55d6f1c63ad81896f2ab842b2
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-05-07 17:59:50 +0200

    Parents created when child references them, IDs read from records are copied into new buffers

--- bioinfo-libs/ped/ped_file.c
+++ bioinfo-libs/ped/ped_file.c
@@ -63,19 +63,20 @@ void ped_record_free(ped_record_t* ped_record) {
 //-----------------------------------------------------
 
 int ped_read(ped_file_t *ped_file) {
+    int ret_code = 0;
     list_t *ped_batches = (list_t*) malloc (sizeof(list_t));
     list_init("batches", 1, 100, ped_batches);
     
-    int ret_code = 0;
 #pragma omp parallel sections
 {
 #pragma omp section
     {
         ret_code = ped_read_batches(ped_batches, 1000, ped_file);
-        
         if (ret_code) {
             LOG_FATAL_F("Error %d while reading the file %s\n", ret_code, ped_file->filename);
         }
+        
+        list_decr_writers(ped_batches);
     }
     
 #pragma omp section
@@ -84,8 +85,9 @@ int ped_read(ped_file_t *ped_file) {
         list_item_t *record_item = NULL;
         while ((item = list_remove_item(ped_batches)) != NULL) {
             ped_batch_t *batch = (ped_batch_t*) item->data_p;
-            while ((record_item = list_remove_item(batch)) != NULL) {
+            while ((record_item = list_remove_item_async(batch)) != NULL) {
                 ret_code &= add_ped_record(record_item->data_p, ped_file);
+                ped_record_free(record_item->data_p);
                 list_item_free(record_item);
             }
             ped_batch_free(batch);
@@ -93,7 +95,6 @@ int ped_read(ped_file_t *ped_file) {
     }
 }
     return ret_code;
-//     return ped_ragel_read(NULL, 1, ped_file);
 }
 
 int ped_read_batches(list_t *batches_list, size_t batch_size, ped_file_t *ped_file) {
@@ -140,43 +141,58 @@ int add_ped_record(ped_record_t* record, ped_file_t *ped_file) {
         return -2;
     }
     
+    int result = 0;
+    char *aux_buffer;
+    
     // Get family or, should it not exist yet, create it
     family_t *family = cp_hashtable_get(ped_file->families, record->family_id);
     if (family == NULL) {
-        family = family_new(record->family_id);
+        aux_buffer = (char*) calloc (strlen(record->family_id)+1, sizeof(char));
+        strncat(aux_buffer, record->family_id, strlen(record->family_id));
+        family = family_new(aux_buffer);
         if (add_family(family, ped_file)) {
             return ALREADY_EXISTING_FAMILY;
         }
     }
     
-    int result = 0;
+    LOG_INFO_F("father id = %s\tmother id = %s\n", record->father_id, record->mother_id);
     
     // Get parents from family or, should they not exist yet, create them
     individual_t *father = NULL, *mother = NULL;
-    if (record->father_id != NULL && family->father != NULL) {
-        if (strcasecmp(family->father->id, record->father_id) == 0) {
-            father = family->father;
+    if (record->father_id != NULL) {
+        if (family->father != NULL) {
+            if (strcasecmp(family->father->id, record->father_id) == 0) {
+                father = family->father;
+            } else {
+                return FATHER_APPEARS_MORE_THAN_ONCE;
+            }
         } else {
-            father = individual_new(record->father_id, -9, MALE, NULL, NULL, family);
+            aux_buffer = (char*) calloc (strlen(record->father_id)+1, sizeof(char));
+            strncat(aux_buffer, record->father_id, strlen(record->father_id));
+            father = individual_new(aux_buffer, -9, MALE, NULL, NULL, family);
             family_set_parent(father, family);
         }
     }
-    if (record->mother_id != NULL && family->mother != NULL) {
-        if (strcasecmp(family->mother->id, record->mother_id) == 0) {
-            mother = family->mother;
+    
+    if (record->mother_id != NULL) {
+        if (family->mother != NULL) {
+            if (strcasecmp(family->mother->id, record->mother_id) == 0) {
+                mother = family->mother;
+            } else {
+                return MOTHER_APPEARS_MORE_THAN_ONCE;
+            }
         } else {
-            mother = individual_new(record->mother_id, -9, FEMALE, NULL, NULL, family);
+            aux_buffer = (char*) calloc (strlen(record->mother_id)+1, sizeof(char));
+            strncat(aux_buffer, record->mother_id, strlen(record->mother_id));
+            mother = individual_new(aux_buffer, -9, FEMALE, NULL, NULL, family);
             family_set_parent(mother, family);
         }
     }
     
-    LOG_DEBUG_F("family->father = NULL? %d, family->mother = NULL? %d\n", family->father == NULL, family->mother == NULL);
-    LOG_DEBUG_F("father = NULL? %d, mother = NULL? %d\n", father == NULL, mother == NULL);
-    
     // Create individual with the information extracted from the PED record
     individual_t *individual = individual_new(record->individual_id, record->phenotype, record->sex, father, mother, family);
     if (father != NULL || mother != NULL) {
-        LOG_DEBUG("** add child\n");
+        LOG_DEBUG_F("** add family %s child\n", family->id);
         family_add_child(individual, family);
     } else {
         LOG_DEBUG_F("** set family %s parent of sex %d\n", family->id, individual->sex);
