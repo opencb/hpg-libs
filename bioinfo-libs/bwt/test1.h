5b58823f1f286579fbca5a83b7d72e42f265f58a
jtarraga <jtarraga@cipf.es>
2012-06-19 16:00:42 +0200

    adds test1.h

---
+++ bioinfo-libs/bwt/test1.h
@@ -0,0 +1,15 @@
+#ifndef TEST1
+#define TEST1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "array_list.h"
+
+#include "fastq_file.h"
+#include "fastq_read.h"
+
+#include "burrows_wheeler.h"
+
+
+#endif // TEST1
