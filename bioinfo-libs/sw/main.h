16f1c80ae40e3f554ce364d4068cc6369c959063
jtarraga <jtarraga@cipf.es>
2012-07-27 10:15:03 +0200

    adds main.h

---
+++ bioinfo-libs/sw/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "macros.h"
+#include "emboss.h"
+#include "sse.h"
+#include "smith_waterman.h"
+//#include "avx.h"
+
+void run_emboss(char *q_filename, char *r_filename, 
+		float gap_open, float gap_extend,
+		float match, float mismatch,
+		char *out_filename);
+
+void run_sse(char *q_filename, char *r_filename, 
+	     float gap_open, float gap_extend, char *matrix_filename,
+	     int batch_size, int num_threads, char *out_filename);
+
+#endif // MAIN_H
