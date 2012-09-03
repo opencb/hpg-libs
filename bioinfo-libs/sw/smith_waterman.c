8d2b9a6e8af9992bbf28d04d5051613f76cae5cf
jtarraga <jtarraga@cipf.es>
2012-09-03 17:26:47 +0200

    completes mqsr function

--- bioinfo-libs/sw/smith_waterman.c
+++ bioinfo-libs/sw/smith_waterman.c
@@ -353,65 +353,241 @@ void smith_waterman_mqmr(char **query_p, char **ref_p, unsigned int num_queries,
 
 //------------------------------------------------------------------------------------
 
-void smith_waterman_mqsr(char **query_p, char *ref_p, unsigned int num_seqs, 
+void smith_waterman_mqsr(char **query_p, char *ref_p, unsigned int num_queries, 
 			 sw_optarg_t *optarg_p, unsigned int num_threads, 
 			 sw_multi_output_t *output_p) {
   
-    if (output_p == NULL) {
-        printf("Error: output buffer is null.\n");
-	exit(-1);
-    }
-    if (output_p->num_queries != num_seqs) {
-        printf("Error: num. sequencias (%i) and output buffer length (%i) mismatch !\n", 
-	       num_seqs, output_p->num_queries);
-	exit(-1);
+  const unsigned int simd_depth = 4;
+
+  if (output_p == NULL) {
+    printf("Error: output buffer is null.\n");
+    exit(-1);
+  }
+  if (output_p->num_queries < num_queries) {
+    printf("Error: num. sequencias (%i) and output buffer length (%i) mismatch !\n", 
+	   num_queries, output_p->num_queries);
+    exit(-1);
+  }
+
+  int ref_len = strlen(ref_p);
+  
+  if (num_threads == 1) {
+
+    double partial_t;
+
+    char *q_aux = NULL, *r_aux = NULL;
+    int depth, aux_size = 0, H_size = 0, F_size = 0, matrix_size = 0, max_q_len = 0, max_r_len = ref_len;
+    char *q[simd_depth], *r[simd_depth];
+    int len, index, q_lens[simd_depth], r_lens[simd_depth], alig_lens[simd_depth];
+    float *H = NULL, *F = NULL;
+    int *C = NULL;
+
+    float gap_open = optarg_p->gap_open, gap_extend = optarg_p->gap_extend;
+    float *score_p = output_p->score_p;
+
+    depth = 0;
+    for (unsigned int i = 0; i < num_queries; i++) {
+
+      len = strlen(query_p[i]);
+      if (len > max_q_len) max_q_len = len;
+      q_lens[depth] = len;
+      q[depth] = query_p[i];
+
+      r_lens[depth] = ref_len;
+      r[depth] = ref_p;
+
+      depth++;
+      if (depth == simd_depth) {
+
+	index = i - depth + 1;
+
+	reallocate_memory(max_q_len, max_r_len, simd_depth, &H_size, &H, &C, &F_size, &F, &aux_size, &q_aux, &r_aux);
+	//printf("-----> max_q_len = %i, max_r_eln = %i, simd_depth = %i\n", max_q_len, max_r_len, simd_depth);
+	//printf("-----> H_size = %i, F_size = %i, aux_size = %i\n", H_size, F_size, aux_size);
+	//printf("num_queries = %i, gap_open = %0.2f, gap_extend = %0.2f\n", depth, gap_open, gap_extend);
+
+	// generating score matrix
+	partial_t = tic();
+	sse_matrix(depth, q, q_lens, max_q_len, r, r_lens, max_r_len, 
+		   optarg_p->subst_matrix, gap_open, gap_extend, H, F, C, &score_p[index]);
+	sse_matrix_t[0] += toc(partial_t);
+
+	// tracebacking
+	partial_t = tic();
+	simd_traceback(simd_depth, depth, q, q_lens, max_q_len, r, r_lens, max_r_len,
+		       gap_open, gap_extend, H, C, &score_p[index],
+		       &output_p->query_map_p[index], &output_p->query_start_p[index],
+		       &output_p->ref_map_p[index], &output_p->ref_start_p[index], alig_lens,
+		       q_aux, r_aux);
+	sse_tracking_t[0] += toc(partial_t);
+	
+	depth = 0;
+      }
     }
 
-    /*
-    unsigned int num_packs = num_seqs / SIMD_DEPTH;
-    if (num_seqs % SIMD_DEPTH) {
+    //    printf("depth = %i\n", depth);
+
+    if (depth > 0) {
+
+      float max_score[simd_depth];
+      
+      for (unsigned int i = depth; i < simd_depth; i++) {
+	q[i] = q[0];
+	q_lens[i] = q_lens[0];
+
+	r[i] = r[0];
+	r_lens[i] = r_lens[0];
+      }
+
+      reallocate_memory(max_q_len, max_r_len, simd_depth, &H_size, &H, &C, &F_size, &F, &aux_size, &q_aux, &r_aux);
+      
+      index = num_queries - depth;
+
+      // generating score matrix
+      partial_t = tic();
+      sse_matrix(depth, q, q_lens, max_q_len, r, r_lens, max_r_len, 
+		 optarg_p->subst_matrix, gap_open, gap_extend, H, F, C, max_score);
+      sse_matrix_t[0] += toc(partial_t);
+
+      // tracebacking
+      partial_t = tic();
+      simd_traceback(simd_depth, depth, q, q_lens, max_q_len, r, r_lens, max_r_len,
+		     gap_open, gap_extend, H, C, max_score,
+		     &output_p->query_map_p[index], &output_p->query_start_p[index],
+		     &output_p->ref_map_p[index], &output_p->ref_start_p[index], alig_lens,
+		     q_aux, r_aux);
+      sse_tracking_t[0] += toc(partial_t);
+
+      for (unsigned int i = 0; i < depth; i++) {
+	score_p[index +  i] = max_score[i];
+      }
+    }
+
+    // free memory
+    if (H != NULL) _mm_free(H);
+    if (C != NULL) _mm_free(C);
+    if (F != NULL) _mm_free(F);
+    if (q_aux != NULL) free(q_aux);
+    if (r_aux != NULL) free(r_aux);
+    
+  } else { // multi-thread
+
+      unsigned int num_packs = num_queries / simd_depth;
+      if (num_queries % simd_depth) {
         num_packs++;
-    }
-    unsigned int packs_per_thread = num_packs / num_threads;
-    if (num_packs % num_threads) {
+      }
+      unsigned int packs_per_thread = num_packs / num_threads;
+      if (num_packs % num_threads) {
         packs_per_thread++;
-    }
+      }
 
-    char* refs[SIMD_DEPTH];
-    for (int i = 0; i < SIMD_DEPTH; i++) {
-        refs[i] = ref_p;
-    }
-
-    #pragma omp parallel num_threads(num_threads)
-    {
+      //printf("num_packs = %i, packs_per_thread = %i\n", num_packs, packs_per_thread);
+  
+      #pragma omp parallel num_threads(num_threads) shared(num_packs, packs_per_thread, optarg_p, output_p)
+      {
+	double partial_t = 0.0;
         unsigned int tid = omp_get_thread_num();
+	
+	unsigned int first_index = tid * packs_per_thread * simd_depth;
+	unsigned int last_index = first_index + (packs_per_thread * simd_depth);
+	if (last_index > num_queries) last_index = num_queries;
 
-	sw_simd_context_t *context_p = sw_simd_context_new(optarg_p->gap_open, optarg_p->gap_extend,
-							   &(optarg_p->subst_matrix));
-							   
-	unsigned int i;
-	unsigned int first_index = tid * packs_per_thread * SIMD_DEPTH;
-	unsigned int last_index = first_index + ((packs_per_thread - 1) * SIMD_DEPTH);
-	if (last_index > num_seqs) last_index = num_seqs;
+	//printf("tid = %i, from %i to %i\n", tid, first_index, last_index);
 
-	for (i = first_index; i < last_index; i += SIMD_DEPTH) {
-  	    smith_waterman_simd(&query_p[i], refs, SIMD_DEPTH,
-				&(output_p->query_map_p[i]), &(output_p->ref_map_p[i]), 
-				&(output_p->query_start_p[i]), &(output_p->ref_start_p[i]),
-				&(output_p->score_p[i]), context_p);
+	char *q_aux = NULL, *r_aux = NULL;
+	int depth, aux_size = 0, H_size = 0, F_size = 0, matrix_size = 0, max_q_len = 0, max_r_len = ref_len;
+	char *q[simd_depth], *r[simd_depth];
+	int len, index, q_lens[simd_depth], r_lens[simd_depth], alig_lens[simd_depth];
+	float *H = NULL, *F = NULL;
+	int *C = NULL;
+	
+	float gap_open = optarg_p->gap_open, gap_extend = optarg_p->gap_extend;
+	float *score_p = output_p->score_p;
+	
+	depth = 0;
+	for (unsigned int i = first_index; i < last_index; i++) {
+	  
+	  len = strlen(query_p[i]);
+	  if (len > max_q_len) max_q_len = len;
+	  q_lens[depth] = len;
+	  q[depth] = query_p[i];
+	  
+	  r_lens[depth] = ref_len;
+	  r[depth] = ref_p;
+	  
+	  depth++;
+	  if (depth == simd_depth) {
+	    
+	    index = i - depth + 1;
+	    
+	    reallocate_memory(max_q_len, max_r_len, simd_depth, &H_size, &H, &C, &F_size, &F, &aux_size, &q_aux, &r_aux);
+	    
+	    // generating score matrix
+	    partial_t = tic();
+	    sse_matrix(depth, q, q_lens, max_q_len, r, r_lens, max_r_len, 
+		       optarg_p->subst_matrix, gap_open, gap_extend, H, F, C, &score_p[index]);
+	    sse_matrix_t[tid] += toc(partial_t);
+	    
+	    // tracebacking
+	    partial_t = tic();
+	    simd_traceback(simd_depth, depth, q, q_lens, max_q_len, r, r_lens, max_r_len,
+			   gap_open, gap_extend, H, C, &score_p[index],
+			   &output_p->query_map_p[index], &output_p->query_start_p[index],
+			   &output_p->ref_map_p[index], &output_p->ref_start_p[index], alig_lens,
+			   q_aux, r_aux);
+	    sse_tracking_t[tid] += toc(partial_t);
+	    
+	    depth = 0;
+	  }
+	}
+	
+	//	printf("depth = %i\n", depth);
+	
+	if (depth > 0) {
+	  
+	  float max_score[simd_depth];
+	  
+	  for (unsigned int i = depth; i < simd_depth; i++) {
+	    q[i] = q[0];
+	    q_lens[i] = q_lens[0];
+	    
+	    r[i] = r[0];
+	    r_lens[i] = r_lens[0];
+	  }
+	  
+	  reallocate_memory(max_q_len, max_r_len, simd_depth, &H_size, &H, &C, &F_size, &F, &aux_size, &q_aux, &r_aux);
+	  
+	  index = num_queries - depth;
+	  
+	  // generating score matrix
+	  partial_t = tic();
+	  sse_matrix(depth, q, q_lens, max_q_len, r, r_lens, max_r_len, 
+		     optarg_p->subst_matrix, gap_open, gap_extend, H, F, C, max_score);
+	  sse_matrix_t[tid] += toc(partial_t);
+	  
+	  // tracebacking
+	  partial_t = tic();
+	  simd_traceback(simd_depth, depth, q, q_lens, max_q_len, r, r_lens, max_r_len,
+			 gap_open, gap_extend, H, C, max_score,
+			 &output_p->query_map_p[index], &output_p->query_start_p[index],
+			 &output_p->ref_map_p[index], &output_p->ref_start_p[index], alig_lens,
+			 q_aux, r_aux);
+	  sse_tracking_t[tid] += toc(partial_t);
+	  
+	  for (unsigned int i = 0; i < depth; i++) {
+	    score_p[index +  i] = max_score[i];
+	  }
 	}
 
-	int num_queries = (i + SIMD_DEPTH > num_seqs) ? (num_seqs - i) : SIMD_DEPTH;
-	if (num_queries > 0) {
-	    smith_waterman_simd(&query_p[i], refs, num_queries,
-				&(output_p->query_map_p[i]), &(output_p->ref_map_p[i]), 
-				&(output_p->query_start_p[i]), &(output_p->ref_start_p[i]),
-				&(output_p->score_p[i]), context_p);
-	}
+	// free memory
+	if (H != NULL) _mm_free(H);
+	if (C != NULL) _mm_free(C);
+	if (F != NULL) _mm_free(F);
+	if (q_aux != NULL) free(q_aux);
+	if (r_aux != NULL) free(r_aux);
 
-	free(context_p);
-    }
-    */
+      } // end #pragma omp parallel
+  }
 }
 
 //------------------------------------------------------------------------------------
