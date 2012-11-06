f4e09b43100537dc2c82297ee415c4f92c98eae3
imedina <imedina@cipf.es>
2012-11-06 01:13:33 +0100

    fastq: some old code and comments removed

--- bioinfo-libs/bioformats/fastq/fastq_read.c
+++ bioinfo-libs/bioformats/fastq/fastq_read.c
@@ -3,14 +3,9 @@
 
 #include "fastq_read.h"
 
-//--------------------------------------------
-//--------------------------------------------
 
 fastq_read_t *fastq_read_new(char *id, char *sequence, char *quality) {
 	fastq_read_t *fq_read = (fastq_read_t*) calloc(1, sizeof(fastq_read_t));
-	/*fq_read->id = strdup(id);
-  fq_read->quality = strdup(quality);
-  fq_read->sequence = strdup(sequence);*/
 
 	size_t id_length = strlen(id); // + 1;
 	size_t seq_length = strlen(sequence); // + 1;
@@ -19,14 +14,6 @@ fastq_read_t *fastq_read_new(char *id, char *sequence, char *quality) {
 	fq_read->sequence = strndup(sequence, seq_length);
 	fq_read->quality = strndup(quality, seq_length);
 	fq_read->length = seq_length;
-
-	//  fq_read->id = (char *)malloc(sizeof(char)*id_length);
-	//  fq_read->sequence = (char *)malloc(sizeof(char)*seq_length);
-	//  fq_read->quality = (char *)malloc(sizeof(char)*seq_length);
-	//
-	//  memcpy(fq_read->id, id, id_length);
-	//  memcpy(fq_read->sequence, sequence, seq_length);
-	//  memcpy(fq_read->quality, quality, seq_length);
 
 	return fq_read;
 }
