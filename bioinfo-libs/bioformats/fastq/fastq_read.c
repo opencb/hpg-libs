c91b64ea3494d537343205b74d5409b9c426bd8e
jtarraga <jtarraga@jtarragaPC.(none)>
2014-05-21 11:47:38 +0200

    Includes adapter support in the fastq_read_t structure

--- bioinfo-libs/bioformats/fastq/fastq_read.c
+++ bioinfo-libs/bioformats/fastq/fastq_read.c
@@ -44,7 +44,13 @@ fastq_read_t *fastq_read_new(char *id, char *sequence, char *quality) {
   //printf("%s\n", fq_read->sequence);
   fq_read->quality = strdup(quality);
   fq_read->length = seq_length;
-  
+
+  fq_read->adapter = NULL;
+  fq_read->adapter_revcomp = NULL;
+  fq_read->adapter_quality = NULL;
+  fq_read->adapter_length = 0;
+  fq_read->adapter_strand = 0;
+
   return fq_read;
 
 }
@@ -56,6 +62,11 @@ fastq_read_t *fastq_read_dup(fastq_read_t *fq) {
   fq_read->revcomp = strdup(fq->revcomp);
   fq_read->quality = strdup(fq->quality);
   fq_read->length = fq->length;
+  if (fq->adapter) fq_read->adapter = strdup(fq->adapter);
+  if (fq->adapter_revcomp) fq_read->adapter_revcomp = strdup(fq->adapter_revcomp);
+  if (fq->adapter_quality) fq_read->adapter_quality = strdup(fq->adapter_quality);
+  fq_read->adapter_length = fq->adapter_length;
+  fq_read->adapter_strand = fq->adapter_strand;
 
   return fq_read;
 }
@@ -91,6 +102,10 @@ void fastq_read_free(fastq_read_t *fq_read) {
 	if (fq_read->revcomp != NULL) free(fq_read->revcomp);
 	if (fq_read->quality != NULL) free(fq_read->quality);
 
+	if (fq_read->adapter != NULL) free(fq_read->adapter);
+	if (fq_read->adapter_revcomp != NULL) free(fq_read->adapter_revcomp);
+	if (fq_read->adapter_quality != NULL) free(fq_read->adapter_quality);
+
 	free(fq_read);
 }
 
@@ -108,6 +123,18 @@ void fastq_read_pe_free(fastq_read_pe_t *fq_read_pe) {
 }
 
 
+
+void fastq_read_display(fastq_read_t *read) {
+	printf("id            : %s\n", read->id);
+	printf("sequence      : %s\n", read->sequence);
+	printf("seq. revcomp. : %s\n", read->revcomp);
+	printf("seq. length   : %i\n", read->length);
+	printf("seq. quality  : %s\n", read->quality);
+	printf("adapter       : %s\n", read->adapter);
+	printf("adap. revcomp.: %s\n", read->adapter_revcomp);
+	printf("adap. length  : %i\n", read->adapter_length);
+	printf("adap. qual.   : %s\n", read->adapter_quality);
+}
 
 void fastq_read_print(fastq_read_t *read) {
 	printf("%s\n", read->id);
