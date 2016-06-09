c90de8bf670157be25fe09289c42da53ec9b419d
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2016-06-09 10:23:58 +0100

    hpg-sw: removes the exit command from compare.sh script

--- c/src/aligners/sw/compare.sh
+++ c/src/aligners/sw/compare.sh
@@ -6,7 +6,6 @@ for simd in SSE AVX2; do
 	for n in 1 2 4 6 8 10 12 14 16; do
 	    echo "running $simd batch-size:$b num-threads:$n"
 	    ./bin/hpg-sw -q /ramdisks/queries.10M.txt -r /ramdisks/refs.10M.txt -d /ramdisks/ -o out.results -s dnafull -b $b -p 10 -e 0.5 -n $n > results/${simd}_${b}_${n}.times
-	    exit
 	done 
     done 
 done 
