d60fc82acd2ab7d4ccf9d61740b91707a8f65c44
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2016-06-09 10:22:39 +0100

    hpg-sw: adds input parameter (compiler) for compare.sh script

--- c/src/aligners/sw/compare.sh
+++ c/src/aligners/sw/compare.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 for simd in SSE AVX2; do
-    make clean ; make METHOD=OMP TIMING=1 SIMD=$simd
+    make clean ; make METHOD=OMP TIMING=1 SIMD=$simd COMPILER=$1
     for b in 128 1024 4096; do
 	for n in 1 2 4 6 8 10 12 14 16; do
 	    echo "running $simd batch-size:$b num-threads:$n"
@@ -12,18 +11,3 @@ for simd in SSE AVX2; do
     done 
 done 
 
-# use predefined variables to access passed arguments
-#echo arguments to the shell
-#echo $1 $2 $3 ' -> echo $1 $2 $3'
-
-# We can also store arguments from bash command line in special array
-args=("$@")
-#echo arguments to the shell
-#echo ${args[0]} ${args[1]} ${args[2]} ' -> args=("$@"); echo ${args[0]} ${args[1]} ${args[2]}'
-
-#use $@ to print out all arguments at once
-#echo $@ ' -> echo $@'
-
-# use $# variable to print out
-# number of arguments passed to the bash script
-#echo Number of arguments passed: $# ' -> echo Number of arguments passed: $#' 
