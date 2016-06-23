4250b7e05f1f1593ab7badd8f67100da72f2e065
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2016-06-23 11:18:27 +0100

    hpg-sw: skip

--- c/src/aligners/sw/compare.sh
+++ c/src/aligners/sw/compare.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-for simd in SSE AVX2 AVX512; do
+#for simd in SSE AVX2 AVX512; do
+for simd in SSE AVX2; do
     make clean ; make TIMING=1 SIMD=$simd COMPILER=$1
     for b in 128 1024 4096; do
 	n=1
 	echo "running $simd batch-size:$b num-threads:$n"
 	./bin/hpg-sw -q $2 -r $3 -s dnafull -b $b -p 10 -e 0.5 -n $n > results/${simd}_${b}_${n}.times
-	for n in {2..272..2}; do
+#	for n in {2..272..2}; do
+	for n in {2..16..2}; do
 	    echo "running $simd batch-size:$b num-threads:$n"
 	    ./bin/hpg-sw -q $2 -r $3 -s dnafull -b $b -p 10 -e 0.5 -n $n > results/${simd}_${b}_${n}.times
 	done 
